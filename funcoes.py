b063d188ebd0c24282f7704ae4a3b6a1b4f01554
julienyinsper <julieny@al.insper.edu.br>
2026-04-30 16:15:58 -0300

    Q2

--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,11 @@ def rolar_dados(numero):
     for i in range(numero):
         lista.append(random.randint(1,6))
     return lista
+
+# Q2
+def guardar_dado(dados_rolados, dados_no_estoque, dados_a_guardar):
+    dado = dados_rolados[dados_a_guardar]
+    dados_no_estoque.append(dado)
+    del dados_rolados[dados_a_guardar]
+    return [dados_rolados, dados_no_estoque]
+    
